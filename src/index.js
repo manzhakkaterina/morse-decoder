ffa811d2df3c10b8e0e9a04cb4a5201951ce6d2b
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-02-13 22:18:25 +0200

    fix: add code

--- src/index.js
+++ src/index.js
@@ -38,6 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+    morse = "";
+res_text = '';
+k = 0;
+for (let i = 0; i < expr.length; i++) {
+      if (expr[i] !== ' ') {
+for (let code in MORSE_TABLE) {
+  if (expr[i].toLowerCase() === MORSE_TABLE[code]) {
+
+        for (let j = 0; j < code.length; j++) {
+      if (code[j] === '.') {
+        res_text = res_text + '10'; k = k + 2;
+      } else {
+        if (code[j] === '-') {
+        res_text = res_text + '11'; k = k + 2;
+      } }
+          }
+            if (k !== 10) {
+          for (let h = 0; h < 10 - k; h++) {
+            res_text = '0' + res_text;
+        }       
+    }
+    k = 0;
+    morse = morse + res_text;
+    } 
+  }
+}
+  else { morse = morse + '**********'; }
+    console.log(res_text);
+  res_text = '';
+}
+  return morse; 
     // write your solution here
 }
 
